c81dfdc68dcc819994ed0031efbc33e39b49a3a8
amin-leon <104311071+amin-leon@users.noreply.github.com>
2023-08-07 15:38:48 +0200

    project setup and tailwind configuration

---
+++ clients/src/index.js
@@ -0,0 +1,12 @@
+// src/index.js or src/index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>
+
+,document.getElementById('root'));
